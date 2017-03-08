0174ece892facd72ed2db4c78431fb1dee6fe5b4
Kevin García <gar13177@uvg.edu.gt>
2017-03-08 00:16:41 -0600

    timeout al socket agregado
    se debe agregar a los clientes para saber cuando se cayó la conexión

--- src/Connection/server1.java
+++ src/Connection/server1.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 import PongV2.Game;
 
@@ -16,14 +17,28 @@ public class server1 implements Runnable {
 	String broadcast = "";
 	int port = 0;
 	Game g;
+	long time;
 	
 	public server1(Game g){
 		this.g = g;
+		this.time = 20000;
 		try {
 			MyService = new ServerSocket(0);//le digo que busque cualquier puerto disponible
+			MyService.setSoTimeout((int)this.time);
 		} catch (IOException e){
 			System.out.println(e);
 		}
+	}
+	
+	public void setTimeOut(long time){
+		this.time = time;
+		if (MyService != null)
+			try {
+				MyService.setSoTimeout((int)time);
+			} catch (SocketException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 	}
 	
 	public int getPort(){//obtengo el puerto en el que me estoy conectando
@@ -54,7 +69,6 @@ public class server1 implements Runnable {
 		if (MyService == null) return;
 		boolean recieve = true;
 		long test = System.currentTimeMillis();
-		long wait = 20000;//espero 20 segundos como maximo
 		int cantidad = 0;
 		while (recieve){
 			//Socket clientSocket = null;
@@ -76,7 +90,7 @@ public class server1 implements Runnable {
 			if (cantidad > 3){//solo acepto hasta 4
 				recieve = false;
 			}
-			if (System.currentTimeMillis() > test+wait){//si ya me pase de la hora
+			if (System.currentTimeMillis() > test+time){//si ya me pase de la hora
 				recieve = false;
 			}
 		}
